c75b7454bc1849ae0590f5d4914387a1b7989918
Daniel Wallace <danielwallace@gtmanfred.com>
2021-06-10 12:39:46 -0500

    Initial Commit

---
+++ src/teststack/__init__.py
@@ -0,0 +1,9 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version(root="..", relative_to=__file__)
+    except ImportError:
+        __version__ = None
