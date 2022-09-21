4365e95900ffda87a7697ea120259cffc4ddf1d8
Daniel Wallace <danielwallace@gtmanfred.com>
2022-09-21 10:55:27 -0400

    make a better context manager
    We want to make sure the reset of the stdin file descriptor is reset
    when a keyboard interupt comes through, and apparently the context
    manager wrapper doesn't do it for us, so context manager class it is.

--- src/teststack/utils.py
+++ src/teststack/utils.py
@@ -4,20 +4,22 @@ import termios
 import tty
 
 
-@contextlib.contextmanager
-def read_from_stdin():
-    if sys.stdin.isatty():  # pragma: no cover
-        fd = sys.stdin.fileno()
-        orig_fl = termios.tcgetattr(fd)
-        tty.setcbreak(fd)  # use tty.setraw() instead to catch ^C also
-        mode = termios.tcgetattr(fd)
-        CC = 6
-        mode[CC][termios.VMIN] = 0
-        mode[CC][termios.VTIME] = 0
-        termios.tcsetattr(fd, termios.TCSAFLUSH, mode)
+class read_from_stdin:
+    def __enter__(self):
+        if sys.stdin.isatty():  # pragma: no cover
+            fd = sys.stdin.fileno()
+            self.orig_fl = termios.tcgetattr(fd)
+            tty.setcbreak(fd)  # use tty.setraw() instead to catch ^C also
+            mode = termios.tcgetattr(fd)
+            CC = 6
+            mode[CC][termios.VMIN] = 0
+            mode[CC][termios.VTIME] = 0
+            termios.tcsetattr(fd, termios.TCSAFLUSH, mode)
 
-        yield fd
+            return fd
 
-        termios.tcsetattr(fd, termios.TCSANOW, orig_fl)
-    else:
-        yield None
+        return None
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        if getattr(self, 'orig_fl', None) is not None:
+            termios.tcsetattr(sys.stdin.fileno(), termios.TCSANOW, self.orig_fl)
